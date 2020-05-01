f69af43dfbd68de3a1a637b57253f90a6a1b11dc
andrusha52 <andrushaes@gmail.com>
2020-05-01 16:57:21 +0300

    Updates

---
+++ precache-manifest.07660e38a82952d3dce816fae8d4f346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad51940cf26c75faad8e61eba104ae40",
+    "url": "/test_apps/index.html"
+  },
+  {
+    "revision": "c6f774dabbcaa75c9e84",
+    "url": "/test_apps/static/css/main.016a2c15.chunk.css"
+  },
+  {
+    "revision": "c41ea188db49827e1f9c",
+    "url": "/test_apps/static/js/2.8adb2ab1.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/test_apps/static/js/2.8adb2ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f774dabbcaa75c9e84",
+    "url": "/test_apps/static/js/main.0f916018.chunk.js"
+  },
+  {
+    "revision": "e54443784a9bbc0dab97",
+    "url": "/test_apps/static/js/runtime-main.8222b4d4.js"
+  }
+]);
